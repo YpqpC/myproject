af996671b68b5a26afb8ebb47a05ad933346e939
YpqpC <130731025+YpqpC@users.noreply.github.com>
2024-07-09 09:42:16 +0800

    Add files via upload

---
+++ assets/book-BzlkI4d6.js
@@ -0,0 +1,1 @@
+const o=""+new URL("book-DSxW1xu8.png",import.meta.url).href;export{o as _};
---
+++ assets/book-legacy-eoFO5Oco.js
@@ -0,0 +1,1 @@
+System.register([],(function(e,t){"use strict";return{execute:function(){e("_",""+new URL("book-DSxW1xu8.png",t.meta.url).href)}}}));
